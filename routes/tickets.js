0864a1333cf2ca0ad2321c835f0d991c6d0c04a1
kmstumpff <kmstumpff@gmail.com>
2014-07-07 18:44:13 -0400

    ticket list

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET ticket listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
